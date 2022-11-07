fd10e6b8db2298d7aa7af4a19f28a6362a0bad3a
mukeunkim <mukeunkim@gmail.com>
2022-11-07 17:17:35 +0900

    Upgrade Agora-cl v1.0.2

--- agora.sh
+++ agora.sh
@@ -96,7 +96,7 @@ elif [ "$1" = "cl-node" ]; then
         -v $(pwd)/root/:/root \
         -p 3500:3500 -p 4000:4000 -p 13000:13000 -p 12000:12000/udp \
         --name cl-node --rm \
-        bosagora/agora-cl-node:v1.0.1 \
+        bosagora/agora-cl-node:v1.0.2 \
         --chain-config-file=/root/config/cl/chain-config.yaml \
         --config-file=/root/config/cl/config.yaml \
         --p2p-host-ip=$(curl -s https://ifconfig.me/ip)
@@ -133,8 +133,9 @@ elif [ "$1" = "validator" ]; then
         docker run -it \
         -v $(pwd)/root/:/root \
         --name cl-validator --rm \
-        bosagora/agora-cl-validator:v1.0.1 \
+        bosagora/agora-cl-validator:v1.0.2 \
         accounts import \
+        --chain-config-file=/root/config/cl/chain-config.yaml \
         --keys-dir=/root/$3 \
         --wallet-dir=/root/wallet
 
@@ -144,8 +145,9 @@ elif [ "$1" = "validator" ]; then
         -v $(pwd)/root/:/root \
         --network=host \
         --name cl-validator --rm \
-        bosagora/agora-cl-validator:v1.0.1 \
+        bosagora/agora-cl-validator:v1.0.2 \
         accounts voluntary-exit \
+        --chain-config-file=/root/config/cl/chain-config.yaml \
         --wallet-dir=/root/wallet \
         --beacon-rpc-provider=127.0.0.1:4000
 
@@ -155,7 +157,7 @@ elif [ "$1" = "validator" ]; then
         -v $(pwd)/root/:/root \
         --network="host" \
         --name cl-validator --rm \
-        bosagora/agora-cl-validator:v1.0.1 \
+        bosagora/agora-cl-validator:v1.0.2 \
         --chain-config-file=/root/config/cl/chain-config.yaml \
         --datadir=/root/chain/cl/ \
         --wallet-dir=/root/wallet \
