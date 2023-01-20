1c30c86cda76a2a43e6b5d3a63dcdaf8d45329f8
mukeunkim <mukeunkim@gmail.com>
2023-01-20 11:10:03 +0900

    Add a script to upgrade

---
+++ upgrade.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/monitoring/dashboard/agora-chain-dashboard.json -O monitoring/dashboard/agora-chain-dashboard.json
+wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/monitoring/prometheus/config.yml -O monitoring/prometheus/config.yml
+wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/root/config/cl/chain-config.yaml -O root/config/cl/chain-config.yaml
+wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/root/config/cl/config.yaml -O root/config/cl/config.yaml
+wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/root/config/el/config.toml -O root/config/el/config.toml
+wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/root/config/el/genesis.json -O root/config/el/genesis.json
+wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/agora.bat -O agora.bat
+wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/agora.sh -O agora.sh
+wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/docker-compose.yml -O docker-compose.yml
+wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/docker-compose-monitoring.yml -O docker-compose-monitoring.yml
+
+chmod agora.sh 755
