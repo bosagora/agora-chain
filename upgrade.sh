c35e976f66a86ea4b119d9f62a8bac2c3d2df0a4
michael.kim <michael.kim.200518@gmail.com>
2023-01-22 21:02:16 +0900

    Update Script

--- upgrade.sh
+++ upgrade.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
-mkdir -p monitoring
-mkdir -p monitoring/dashboard
-mkdir -p monitoring/prometheus
-mkdir -p root
-mkdir -p root/config
-mkdir -p root/config/cl
-mkdir -p root/config/el
+function create_folder() {
+  mkdir -p "$@"
+}
 
-wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/monitoring/dashboard/agora-chain-dashboard.json -O monitoring/dashboard/agora-chain-dashboard.json
-wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/monitoring/prometheus/config.yml -O monitoring/prometheus/config.yml
-wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/root/config/cl/chain-config.yaml -O root/config/cl/chain-config.yaml
-wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/root/config/cl/config.yaml -O root/config/cl/config.yaml
-wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/root/config/el/config.toml -O root/config/el/config.toml
-wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/root/config/el/genesis.json -O root/config/el/genesis.json
-wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/agora.bat -O agora.bat
-wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/agora.sh -O agora.sh
-wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/docker-compose.yml -O docker-compose.yml
-wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/docker-compose-monitoring.yml -O docker-compose-monitoring.yml
+function download_file() {
+  wget https://raw.githubusercontent.com/bosagora/agora-chain/mainnet/$@ -O $@
+}
 
-chmod agora.sh 755
+create_folder monitoring
+create_folder monitoring/dashboard
+create_folder monitoring/prometheus
+create_folder root
+create_folder root/config
+create_folder root/config/cl
+create_folder root/config/el
+
+download_file monitoring/dashboard/agora-chain-dashboard.json
+download_file monitoring/prometheus/config.yml
+download_file root/config/cl/chain-config.yaml
+download_file root/config/cl/config.yaml
+download_file root/config/el/config.toml
+download_file root/config/el/genesis.json
+download_file agora.bat
+download_file agora.sh
+download_file docker-compose.yml
+download_file docker-compose-monitoring.yml
+
+chmod 755 agora.sh
