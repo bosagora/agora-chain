5da71e356b759d6f4d939805ebc39f1e8c238b52
Michael Kim <michael.kim.200518@gmail.com>
2023-06-12 09:08:54 +0900

    Add the ability to automatically install required files

--- agora.bat
+++ agora.bat
@@ -9,6 +9,91 @@ set DEVNET=devnet
 set current_path=%cd%
 set script_path=%~dp0
 
+if not exist "networks" (
+
+  echo Starts install ...
+
+  echo Creating folds ...
+  call :createFolder networks
+  call :createFolder networks\mainnet
+  call :createFolder networks\testnet
+  call :createFolder networks\devnet
+
+  call :createFolder networks\mainnet\monitoring
+  call :createFolder networks\mainnet\monitoring\dashboard
+  call :createFolder networks\mainnet\monitoring\prometheus
+  call :createFolder networks\mainnet\root
+  call :createFolder networks\mainnet\root\config
+  call :createFolder networks\mainnet\root\config\cl
+  call :createFolder networks\mainnet\root\config\el
+
+  call :createFolder networks\testnet\monitoring
+  call :createFolder networks\testnet\monitoring\dashboard
+  call :createFolder networks\testnet\monitoring\prometheus
+  call :createFolder networks\testnet\root
+  call :createFolder networks\testnet\root\config
+  call :createFolder networks\testnet\root\config\cl
+  call :createFolder networks\testnet\root\config\el
+
+  call :createFolder networks\devnet\monitoring
+  call :createFolder networks\devnet\monitoring\dashboard
+  call :createFolder networks\devnet\monitoring\prometheus
+  call :createFolder networks\devnet\root
+  call :createFolder networks\devnet\root\config
+  call :createFolder networks\devnet\root\config\cl
+  call :createFolder networks\devnet\root\config\el
+
+  echo Downloading files used on the main network ...
+  call :downloadFile networks/mainnet/monitoring/dashboard/agora-chain-dashboard.json
+  call :downloadFile networks/mainnet/monitoring/prometheus/config.yml
+  call :downloadFile networks/mainnet/root/config/cl/chain-config.yaml
+  call :downloadFile networks/mainnet/root/config/cl/config.yaml
+  call :downloadFile networks/mainnet/root/config/cl/password.txt
+  call :downloadFile networks/mainnet/root/config/cl/proposer_config.json
+  call :downloadFile networks/mainnet/root/config/el/config.toml
+  call :downloadFile networks/mainnet/root/config/el/genesis.json
+  call :downloadFile networks/mainnet/agora.bat
+  call :downloadFile networks/mainnet/agora.sh
+  call :downloadFile networks/mainnet/docker-compose.yml
+  call :downloadFile networks/mainnet/docker-compose-monitoring.yml
+
+  echo Downloading files used on the test network ...
+  call :downloadFile networks/testnet/monitoring/dashboard/agora-chain-dashboard.json
+  call :downloadFile networks/testnet/monitoring/prometheus/config.yml
+  call :downloadFile networks/testnet/root/config/cl/chain-config.yaml
+  call :downloadFile networks/testnet/root/config/cl/config.yaml
+  call :downloadFile networks/testnet/root/config/cl/password.txt
+  call :downloadFile networks/testnet/root/config/cl/proposer_config.json
+  call :downloadFile networks/testnet/root/config/el/config.toml
+  call :downloadFile networks/testnet/root/config/el/genesis.json
+  call :downloadFile networks/testnet/agora.bat
+  call :downloadFile networks/testnet/agora.sh
+  call :downloadFile networks/testnet/docker-compose.yml
+  call :downloadFile networks/testnet/docker-compose-monitoring.yml
+
+  echo Downloading files used on the development network ...
+  call :downloadFile networks/devnet/monitoring/dashboard/agora-chain-dashboard.json
+  call :downloadFile networks/devnet/monitoring/prometheus/config.yml
+  call :downloadFile networks/devnet/root/config/cl/chain-config.yaml
+  call :downloadFile networks/devnet/root/config/cl/config.yaml
+  call :downloadFile networks/devnet/root/config/cl/password.txt
+  call :downloadFile networks/devnet/root/config/cl/proposer_config.json
+  call :downloadFile networks/devnet/root/config/el/config.toml
+  call :downloadFile networks/devnet/root/config/el/genesis.json
+  call :downloadFile networks/devnet/agora.bat
+  call :downloadFile networks/devnet/agora.sh
+  call :downloadFile networks/devnet/docker-compose.yml
+  call :downloadFile networks/devnet/docker-compose-monitoring.yml
+
+  call :downloadFile agora.bat
+  call :downloadFile agora.sh
+
+  echo Completed install ...
+
+  goto :end
+
+)
+
 call :getNetwork
 
 if "%~1" == "network" (
@@ -67,6 +152,16 @@ if not exist ".network" (
 if "%network%" == "" (
   set network=%MAINNET%
 )
+goto :end
+
+:createFolder
+if not exist %~1 (
+    mkdir %~1
+)
+goto :end
+
+:downloadFile
+curl https://raw.githubusercontent.com/bosagora/agora-chain/v0.x.x/%~1 -f -s -S -L -o %~1
 goto :end
 
 :end
--- agora.sh
+++ agora.sh
@@ -39,6 +39,105 @@ function getNetwork() {
 
 }
 
+function createFolder() {
+  mkdir -p "$@"
+}
+
+function downloadFile() {
+  wget https://raw.githubusercontent.com/bosagora/agora-chain/v0.x.x/"$*" -q -O "$*"
+}
+
+if [ ! -d "networks" ]
+then
+
+  echo "Starts install ..."
+
+  echo "Creating folds ..."
+  createFolder networks
+  createFolder networks/mainnet
+  createFolder networks/testnet
+  createFolder networks/devnet
+
+  createFolder networks/mainnet/monitoring
+  createFolder networks/mainnet/monitoring/dashboard
+  createFolder networks/mainnet/monitoring/prometheus
+  createFolder networks/mainnet/root
+  createFolder networks/mainnet/root/config
+  createFolder networks/mainnet/root/config/cl
+  createFolder networks/mainnet/root/config/el
+
+  createFolder networks/testnet/monitoring
+  createFolder networks/testnet/monitoring/dashboard
+  createFolder networks/testnet/monitoring/prometheus
+  createFolder networks/testnet/root
+  createFolder networks/testnet/root/config
+  createFolder networks/testnet/root/config/cl
+  createFolder networks/testnet/root/config/el
+
+  createFolder networks/devnet/monitoring
+  createFolder networks/devnet/monitoring/dashboard
+  createFolder networks/devnet/monitoring/prometheus
+  createFolder networks/devnet/root
+  createFolder networks/devnet/root/config
+  createFolder networks/devnet/root/config/cl
+  createFolder networks/devnet/root/config/el
+
+  echo "Downloading files used on the main network ..."
+  downloadFile networks/mainnet/monitoring/dashboard/agora-chain-dashboard.json
+  downloadFile networks/mainnet/monitoring/prometheus/config.yml
+  downloadFile networks/mainnet/root/config/cl/chain-config.yaml
+  downloadFile networks/mainnet/root/config/cl/config.yaml
+  downloadFile networks/mainnet/root/config/cl/password.txt
+  downloadFile networks/mainnet/root/config/cl/proposer_config.json
+  downloadFile networks/mainnet/root/config/el/config.toml
+  downloadFile networks/mainnet/root/config/el/genesis.json
+  downloadFile networks/mainnet/agora.bat
+  downloadFile networks/mainnet/agora.sh
+  downloadFile networks/mainnet/docker-compose.yml
+  downloadFile networks/mainnet/docker-compose-monitoring.yml
+
+  echo "Downloading files used on the test network ..."
+  downloadFile networks/testnet/monitoring/dashboard/agora-chain-dashboard.json
+  downloadFile networks/testnet/monitoring/prometheus/config.yml
+  downloadFile networks/testnet/root/config/cl/chain-config.yaml
+  downloadFile networks/testnet/root/config/cl/config.yaml
+  downloadFile networks/testnet/root/config/cl/password.txt
+  downloadFile networks/testnet/root/config/cl/proposer_config.json
+  downloadFile networks/testnet/root/config/el/config.toml
+  downloadFile networks/testnet/root/config/el/genesis.json
+  downloadFile networks/testnet/agora.bat
+  downloadFile networks/testnet/agora.sh
+  downloadFile networks/testnet/docker-compose.yml
+  downloadFile networks/testnet/docker-compose-monitoring.yml
+
+  echo "Downloading files used on the development network ..."
+  downloadFile networks/devnet/monitoring/dashboard/agora-chain-dashboard.json
+  downloadFile networks/devnet/monitoring/prometheus/config.yml
+  downloadFile networks/devnet/root/config/cl/chain-config.yaml
+  downloadFile networks/devnet/root/config/cl/config.yaml
+  downloadFile networks/devnet/root/config/cl/password.txt
+  downloadFile networks/devnet/root/config/cl/proposer_config.json
+  downloadFile networks/devnet/root/config/el/config.toml
+  downloadFile networks/devnet/root/config/el/genesis.json
+  downloadFile networks/devnet/agora.bat
+  downloadFile networks/devnet/agora.sh
+  downloadFile networks/devnet/docker-compose.yml
+  downloadFile networks/devnet/docker-compose-monitoring.yml
+
+  downloadFile agora.bat
+  downloadFile agora.sh
+
+  chmod 755 networks/mainnet/agora.sh
+  chmod 755 networks/testnet/agora.sh
+  chmod 755 networks/devnet/agora.sh
+  chmod 755 agora.sh
+
+  echo "Completed install ..."
+
+  exit
+
+fi
+
 getNetwork
 
 if [ "$1" = "network" ]
